9d156bbf3ccb402bd192f514c4e4d1c7c4d94e93
maksim <mmtretiak@gmail.com>
2018-05-19 11:56:10 +0300

    Initial commit

---
+++ client/client.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+	"bytes"
+	"encoding/json"
+)
+
+const url = "http://localhost:8082/"
+
+
+var (
+	MaxWorkers = 4
+	MaxQueue   = 27
+	count      = 0
+	id         = 0
+)
+
+type Foo struct {
+	Key   int `json:"key"`
+	Value int    `json:"value"`
+}
+
+func (foo Foo) do(client http.Client) {
+	var jsonStr, err = json.Marshal(foo)
+	if err != nil {
+		panic(err)
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+}
+
+type Job struct {
+	Foo Foo
+}
+
+var JobQueue chan Job
+
+type Worker struct {
+	WorkerPool chan chan Job
+	JobChannel chan Job
+	quit       chan bool
+	id 		   int
+	Client     http.Client
+}
+
+func NewWorker(workerPool chan chan Job) Worker {
+	id++
+	return Worker{
+		WorkerPool: workerPool,
+		JobChannel: make(chan Job),
+		quit:       make(chan bool),
+		id: 		id,
+		Client:     http.Client{}}
+}
+
+func (w Worker) Start() {
+	go func() {
+		for {
+			// register the current worker into the worker queue.
+			w.WorkerPool <- w.JobChannel
+			select {
+			case job := <-w.JobChannel:
+				// we have received a work request.
+				job.Foo.do(w.Client)
+			case <-w.quit:
+				// we have received a signal to stop
+				return
+			}
+		}
+	}()
+}
+
+func (w Worker) Stop() {
+	go func() {
+		w.quit <- true
+	}()
+}
+
+func main() {
+
+	var rateLimit int
+	var countOfRequests int
+
+	fmt.Print("Enter count of worker: ")
+	fmt.Scanf("%d", &MaxWorkers)
+	fmt.Print("Enter size of queue: ")
+	fmt.Scanf("%d", &MaxQueue)
+
+	JobQueue = make(chan Job, MaxQueue)
+	fmt.Print("Enter count of requests per minute: ")
+	fmt.Scanf("%d", &rateLimit)
+	fmt.Print("Enter count of total requests: ")
+	fmt.Scanf("%d", &countOfRequests)
+
+	t := float64(60.0 / float64(rateLimit))
+	t1 := strconv.FormatFloat(t, 'f', 8, 64) + "s"
+	timeout, err := (time.ParseDuration(t1))
+
+	fmt.Println(t1)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(timeout)
+
+	fmt.Println(time.Now())
+
+	dispatcher := NewDispatcher(MaxWorkers)
+	dispatcher.Run()
+
+
+	for i := 0; i < countOfRequests; i++{
+		go func() {
+			foo := &Foo{Key: i, Value: i}
+			work := Job{Foo: *foo}
+			JobQueue <- work
+
+		}()
+		time.Sleep(timeout)
+	}
+
+	fmt.Println(time.Now())
+}
+
+func send(client http.Client, foo *Foo) {
+
+	var jsonStr, err = json.Marshal(foo)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+}
+
+type Dispatcher struct {
+	WorkerPool chan chan Job
+	maxWorkers int
+}
+
+func NewDispatcher(maxWorkers int) *Dispatcher {
+	pool := make(chan chan Job, maxWorkers)
+	return &Dispatcher{WorkerPool: pool, maxWorkers: maxWorkers}
+}
+
+func (d *Dispatcher) Run() {
+	for i := 0; i < d.maxWorkers; i++ {
+		worker := NewWorker(d.WorkerPool)
+		worker.Start()
+	}
+
+	go d.dispatch()
+}
+
+func (d *Dispatcher) dispatch() {
+
+	for {
+		select {
+		case job := <-JobQueue:
+			go func(job Job) {
+				jobChannel := <-d.WorkerPool
+				jobChannel <- job
+			}(job)
+		}
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"encoding/json"
+	"fmt"
+)
+
+var (
+	MaxWorkers = 0
+	MaxQueue   = 0
+	count      = 0
+	id         = 0
+)
+
+type Foo struct {
+	Key   int `json:"key"`
+	Value int    `json:"value"`
+}
+
+type Job struct {
+	Foo Foo
+}
+
+func (f *Foo) do() {
+	log.Println(f, count)
+	count ++
+}
+
+func fooHandler(rw http.ResponseWriter, req *http.Request) {
+
+	decoder := json.NewDecoder(req.Body)
+	var foo Foo
+	err := decoder.Decode(&foo)
+	if err != nil {
+		panic(err)
+	}
+	defer req.Body.Close()
+
+	go func(foo Foo) {
+		work := Job{foo}
+
+		JobQueue <- work
+	}(foo)
+
+}
+
+var JobQueue chan Job
+
+type Worker struct {
+	WorkerPool chan chan Job
+	JobChannel chan Job
+	quit       chan bool
+	id 		   int
+}
+
+func NewWorker(workerPool chan chan Job) Worker {
+	id++
+	return Worker{
+		WorkerPool: workerPool,
+		JobChannel: make(chan Job),
+		quit:       make(chan bool),
+		id: 		id}
+}
+
+func (w Worker) Start() {
+	go func() {
+		for {
+			// register the current worker into the worker queue.
+			w.WorkerPool <- w.JobChannel
+			select {
+			case job := <-w.JobChannel:
+				// we have received a work request.
+				go job.Foo.do()
+			case <-w.quit:
+				// we have received a signal to stop
+				return
+			}
+		}
+	}()
+}
+
+func (w Worker) Stop() {
+	go func() {
+		w.quit <- true
+	}()
+}
+
+func main() {
+	fmt.Print("Enter count of worker: ")
+	fmt.Scanf("%d", &MaxWorkers)
+	fmt.Print("Enter size of queue: ")
+	fmt.Scanf("%d", &MaxQueue)
+	
+	JobQueue = make(chan Job, MaxQueue)
+
+	log.Println("main start")
+
+	dispatcher := NewDispatcher(MaxWorkers)
+	dispatcher.Run()
+
+	http.HandleFunc("/", fooHandler)
+
+	err := http.ListenAndServe(":8082", nil)
+	if err != nil {
+		fmt.Println("starting listening for payload messages")
+	} else {
+		fmt.Errorf("an error occured while starting payload server %s", err.Error())
+	}
+}
+
+
